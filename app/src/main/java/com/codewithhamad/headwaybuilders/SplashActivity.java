4aee898c494adc43eda5f726bc9d45cd1f1b55b5
Waiz-Z <samowaiz7@gmail.com>
2021-03-24 19:43:30 +0500

    NEW FRONT END

--- app/src/main/java/com/codewithhamad/headwaybuilders/SplashActivity.java
+++ app/src/main/java/com/codewithhamad/headwaybuilders/SplashActivity.java
@@ -1,19 +1,40 @@
 package com.codewithhamad.headwaybuilders;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+
+import com.codewithhamad.headwaybuilders.analyst.AnalystActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
+
+    private static int SPLASHSCREEN=1300;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               Intent intent = new Intent(SplashActivity.this, AnalystActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASHSCREEN);
 
     }
+
 }
